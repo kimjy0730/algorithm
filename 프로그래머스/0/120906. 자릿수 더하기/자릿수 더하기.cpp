66cbb6c29f0cb4213c3384907705290087cf9a26
kimjy0730 <kimjy2767@naver.com>
2024-12-31 09:49:03 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string num = to_string(n);
+    for(int i=0 ; i < num.size();i++)
+    { 
+     answer += num[i]-'0';
+    }
+    return answer;
+}

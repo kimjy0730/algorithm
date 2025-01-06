7c18cb1a725269abcf8f159d18ddcfa3f12206eb
kimjy0730 <kimjy2767@naver.com>
2025-01-06 09:09:04 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string message) {
+    int answer = 0;
+    vector<string>ret ={"message"}  ;
+    answer = message.length()*2;   
+    return answer;
+}

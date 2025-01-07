a2d85aca10b9feadc4f35436a05358bce806168c
kimjy0730 <kimjy2767@naver.com>
2025-01-07 10:48:19 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    sort(array.begin(), array.end());
+    int answer = 0;
+    
+    return answer = array[(array.size()/2) ];
+}

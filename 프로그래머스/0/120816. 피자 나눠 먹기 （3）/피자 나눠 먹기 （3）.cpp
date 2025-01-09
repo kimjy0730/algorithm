e6b034a29e51fe1d569018d2a990e8ad48a30c41
kimjy0730 <kimjy2767@naver.com>
2025-01-09 10:07:52 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int solution(int slice, int n) {
+    int answer = 0;
+
+    return answer = (n + slice - 1) / slice ;
+}

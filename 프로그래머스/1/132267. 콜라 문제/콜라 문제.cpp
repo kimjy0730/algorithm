2b28d3fff8b3593c6cee11b8fa3625f1a901479c
kimjy0730 <kimjy2767@naver.com>
2025-04-11 20:30:16 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+
+    while (n >= a) {
+        int exchanged = (n / a) * b;
+        answer += exchanged;
+        n = (n % a) + exchanged;
+    }
+
+    return answer;
+}
